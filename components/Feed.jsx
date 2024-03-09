89094fc47ee41a5cf74f63fc95c9a34e3d8d6717
mainmann <quanchi1249@gmail.com>
2024-03-10 02:43:48 +0500

    Added full scale Search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,15 @@
 "use client";
-
+//Importing state
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
+//--
 const PromptCardList = ({ data, handleTagClick }) => {
+  // Check if data is an array before using map
+  if (!Array.isArray(data)) {
+    return null; // or render an appropriate fallback UI
+  }
+
   return (
     <div className="mt-16 prompt_layout ">
       {data.map((post) => (
@@ -14,21 +20,52 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setPosts(data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-
-      setPosts(data);
-    };
-    console.log(posts);
     fetchPosts();
   }, []);
+
+  const filteredPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i");
+    const regexTag = new RegExp(`#${searchText.slice(1)}`, "i");
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt) ||
+        regexTag.test(item.tag)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchedResults = filteredPrompts(e.target.value);
+        setSearchedResults(searchedResults);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    // when tag is clicked, update searchText
+    setSearchText(tagName);
+    const searchedResults = filteredPrompts(tagName);
+    setSearchedResults(searchedResults);
+  };
+
   return (
     <section className="feed">
       <form className="relative flex-center w-full">
@@ -41,7 +78,9 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+
+      {/* Render PromptCardList based on searchText */}
+      <PromptCardList data={searchText ? searchedResults : posts} handleTagClick={handleTagClick} />
     </section>
   );
 };
